a19d5999c262e8ec85f52110e06f392b4099c979
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-24 04:36:19 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ use compiler::compiler;
 
 mod bytecode;
 mod compiler;
+mod context;
+mod errors;
+mod formatting;
 mod parser;
+mod stack;
 mod tokens;
 mod transpiler;
+mod utils;
+mod values;
 mod vm;
+use vm::core;
 // mod transpiler;
 
 // use std::env::args;
@@ -18,5 +25,8 @@ fn main() {
   let source = std::fs::read_to_string(&filename).expect("could not read file");
   let arena_allocator = oxc_allocator::Allocator::default();
   let program = compiler(&arena_allocator, &source);
-  println!("{:?}", program);
+
+  let mut ctx = context::Context::new();
+  let result = core::Engine::bootstrap(&mut ctx, &program);
+  println!("{:?}", result);
 }
