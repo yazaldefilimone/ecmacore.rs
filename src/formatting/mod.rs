593e2e6a008ce75f3443b85717c137415d9d64e1
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-24 04:37:02 +0200

    *

---
+++ src/formatting/mod.rs
@@ -0,0 +1,15 @@
+use crate::values::Value;
+use std::fmt;
+use std::fmt::Display;
+
+impl Display for Value {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match &self {
+      Value::String(s) => write!(f, "{}", s),
+      Value::Boolean(b) => write!(f, "{}", b),
+      // Value::Float(f) => write!(f, "{}", f),
+      Value::EOL => write!(f, "End of Line"),
+      _ => write!(f, "Unknown Value"),
+    }
+  }
+}
