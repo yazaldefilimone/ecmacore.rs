cfc628b944bef36d3450abea324a787ae4d5eb35
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-24 04:36:57 +0200

    *

---
+++ src/errors/mod.rs
@@ -0,0 +1,23 @@
+use core::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+pub enum EngineError {
+  StackUnderflow,
+  Other(Box<dyn Error + 'static>),
+}
+
+pub fn other<E: Error + 'static>(e: E) -> EngineError {
+  EngineError::Other(Box::new(e))
+}
+
+pub type Result<T> = std::result::Result<T, EngineError>;
+
+impl fmt::Display for EngineError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      EngineError::StackUnderflow => write!(f, "Stack Underflow"),
+      EngineError::Other(e) => write!(f, "{e}"),
+    }
+  }
+}
