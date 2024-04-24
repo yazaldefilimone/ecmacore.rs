ddc23efd9a2b6cb437874e5e545db7f86099a6b8
Yazalde Filimone <yazaldefilimone@gmail.com>
2024-04-24 04:37:05 +0200

    *

---
+++ src/stack/mod.rs
@@ -0,0 +1,22 @@
+use crate::errors::EngineError;
+use crate::values::Value;
+
+pub struct Stack {
+  stack: Vec<Value>,
+}
+
+impl Stack {
+  pub fn new(capacity: usize) -> Self {
+    Self { stack: Vec::with_capacity(capacity) }
+  }
+
+  #[inline(always)]
+  pub fn push(&mut self, value: Value) {
+    self.stack.push(value);
+  }
+
+  #[inline(always)]
+  pub fn pop(&mut self) -> Result<Value, EngineError> {
+    self.stack.pop().ok_or(EngineError::StackUnderflow)
+  }
+}
