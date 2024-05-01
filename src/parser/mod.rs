60050b045f413af366882fde751efd3db849374b
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-01 19:04:44 +0200

    refactor: dead code

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,10 +1,19 @@
 mod lexer;
-use lexer::Lexer;
+// use lexer::Lexer;
 
-use crate::new_lexer;
+// use crate::new_lexer;
 
-new_lexer!(Parser);
+// new_lexer!(Parser);
 
-impl<'i> Parser<'i> {
-  pub fn parse(&mut self) {}
-}
+// impl<'i> Parser<'i> {
+//   pub fn parse(&mut self) {}
+// }
+
+// use oxc_allocator::Allocator;
+// use oxc_parser::ParserReturn;
+// use oxc_span::SourceType;
+
+// pub fn parser<'a>(arena_allocator: &'a Allocator, source: &'a String) -> ParserReturn<'a> {
+//   let parser = oxc_parser::Parser::new(&arena_allocator, source, SourceType::default());
+//   parser.parse()
+// }
