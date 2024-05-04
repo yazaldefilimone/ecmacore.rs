232a710ac6f26c9a453825d3d3c8615c12d2ca49
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-04 18:58:59 +0200

    feat: disassembler mod

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// use std::env::args;
-
+use std::env::args;
 mod bytecode;
 mod checker;
 mod compiler;
 mod context;
+mod disassembler;
 mod errors;
 mod formatting;
 mod parser;
@@ -12,15 +12,15 @@ mod utils;
 mod values;
 mod vm;
 use vm::core;
-// mod transpiler;
 
-// use std::env::args;
-
-// use transpiler::parse;
 fn main() {
-  // let filename = args().nth(1).expect("no filename given");
-  // let source = std::fs::read_to_string(&filename).expect("could not read file");
-  let source = String::from("30 + 30");
+  let command = args().nth(1);
+  let source = if Some("run") != command.as_deref() {
+    String::from("30 + 30")
+  } else {
+    let filename = args().nth(2).expect("no filename provided");
+    std::fs::read_to_string(&filename).expect("could not read file")
+  };
   let mut ctx = context::Context::new();
   let result = core::Engine::bootstrap(&mut ctx, &source);
   println!("{:?}", result);
