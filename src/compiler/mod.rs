d4ebdb7bb86a4181e1fc651b4ed7ffc72be1504a
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-05 19:29:34 +0200

    fix: source type focus in ts

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -8,7 +8,8 @@ use crate::context::Context;
 use self::compiler::CompilerReturn;
 
 pub fn compile(arena_allocator: &Allocator, source: &String, ctx: &mut Context) -> CompilerReturn {
-  let parser = oxc_parser::Parser::new(&arena_allocator, source, SourceType::default());
+  let source_type = SourceType::default().with_module(true).with_typescript(true);
+  let parser = oxc_parser::Parser::new(&arena_allocator, source, source_type);
   let result = parser.parse();
   Compiler::compile(&result.program, ctx)
 }
