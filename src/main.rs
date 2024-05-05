08c6c8f351f881f2d02344de46243bfe93feb7f0
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-05 05:29:25 +0200

    feat: add `--debug` flag

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,19 @@ use vm::core;
 
 fn main() {
   let command = args().nth(1);
+  let flag = args().nth(2).unwrap_or(String::from(""));
   let source = if Some("run") != command.as_deref() {
     String::from("30 + 30")
   } else {
-    let filename = args().nth(2).expect("no filename provided");
+    let flag = args().nth(2).expect("no filename provided");
+    let filename = if flag.starts_with("-") {
+      args().nth(3).expect("no filename provided")
+    } else {
+      flag
+    };
     std::fs::read_to_string(&filename).expect("could not read file")
   };
   let mut ctx = context::Context::new();
-  let result = core::Engine::bootstrap(&mut ctx, &source);
+  let result = core::Engine::bootstrap(&mut ctx, &source, flag == "--debug");
   println!("{:?}", result);
 }
