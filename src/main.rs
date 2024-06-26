b04198a333294c963e851dfd01864cfbc57b78a2
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-27 01:51:51 +0200

    *

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
+/*
+Copyright 2024 Yazalde Filimone <yazaldefilimon@gmail.com>
+
+*/
+
+// modules
+mod assembler;
 mod bytecode;
-mod checker;
+mod cli;
 mod compiler;
 mod context;
+mod diagnostics;
 mod disassembler;
 mod errors;
 mod formatting;
+mod gc;
 mod parser;
 mod stack;
 mod utils;
 mod values;
 mod vm;
-use vm::core;
-mod cli;
+//  uses
 use cli::command_line;
+use vm::core;
+
+fn run(source: String, is_debug: bool) {
+  let mut ctx = context::Context::new();
+  let result = core::Engine::bootstrap(&mut ctx, &source, is_debug);
+  println!("{:?}", result);
+}
 
 fn main() {
   let matches = command_line();
+
   match matches.subcommand() {
     Some(("run", matches)) => {
       let file = matches.get_one::<String>("file").unwrap();
@@ -33,10 +49,4 @@ fn main() {
       panic!("Unknown command");
     }
   }
-}
-
-fn run(source: String, is_debug: bool) {
-  let mut ctx = context::Context::new();
-  let result = core::Engine::bootstrap(&mut ctx, &source, is_debug);
-  println!("{:?}", result);
 }
