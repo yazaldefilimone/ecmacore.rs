b3d60378c578730603dce8e41e7b84e4a5c32733
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-06-27 01:51:51 +0200

    *

---
+++ src/values/null.rs
@@ -0,0 +1,17 @@
+/*
+Copyright 2024 Yazalde Filimone <yazaldefilimon@gmail.com>
+
+
+*/
+
+/*
+*/
+
+#[derive(Debug)]
+pub struct NullValue;
+
+impl NullValue {
+  pub fn new() -> Self {
+    NullValue
+  }
+}
